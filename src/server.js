f2a362653eb846ae49626e46f3c3ca97059e3a3f
JHKIMS <wjdgns5131@gmail.com>
2023-02-22 15:07:23 +0900

    JsTube: Response생성

--- src/server.js
+++ src/server.js
@@ -3,8 +3,14 @@ import express from "express";
 const app = express();
 const PORT = 4000;
 
-const handleHome = () => { console.log("Trying to go home")}
+const handleHome = (req, res) => { 
+    return res.end("Response Here");
+}
+const handleLogin = (req, res) => {
+    return res.end("Login Here");
+}
 app.get("/", handleHome);
+app.get("/login", handleLogin);
 
 const handleListening = () => {
     console.log(`🔨 Server Listen http://localhost:${PORT} 🐳`)
