04edf21b01f64c946b3674570e88e4470faf43ad
JHKIMS <wjdgns5131@gmail.com>
2023-02-22 16:27:59 +0900

    JsTube: Morgan Middleware 설치

--- src/server.js
+++ src/server.js
@@ -1,23 +1,13 @@
 import express from "express";
+import morgan from "morgan";
 
-const app = express();
 const PORT = 4000;
 
-const loggerMiddleware = (req, res, next) => {
-    console.log(`Someone ${req.method} ${req.url}`)
-    next();
-}
-const protectMiddleware = (req,res,next) => {
-    const url = req.url;
-    if(url === "/protected"){
-        return res.send("<h1>Not Allowed</h1>")
-    }
-    console.log("Passed")
-    next();
-}
+const app = express();
+const morganMiddleware = morgan("dev");
 
 const controllerHome = (req, res) => { 
-    return res.end("Response Here");
+    return res.end("Response Here Home");
 }
 const controllerLogin = (req, res) => {
     return res.end("Login Here");
@@ -26,11 +16,9 @@ const controllerProtect = (req, res) =>{
     return res.end("Welcome to Protected Lounge")
 }
 
-app.use(loggerMiddleware);
-app.use(protectMiddleware);
+app.use(morganMiddleware);
 app.get("/", controllerHome);
 app.get("/login", controllerLogin);
-app.get("/protected", controllerProtect);
 
 const handleListening = () => {
     console.log(`🔨 Server Listen http://localhost:${PORT} 🐳`)
