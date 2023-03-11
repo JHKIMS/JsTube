df07f57a3214543fa61bd522e5bf494f878283fd
JHKIMS <wjdgns5131@gmail.com>
2023-03-11 20:32:35 +0900

    JsTube: Router만들기

---
+++ Readme.md
@@ -0,0 +1,16 @@
+# Clone
+
+URL 디자인<br>
+/ : Home<br>
+/join : Join<br>
+/login : Login<br>
+/search : Search<br>
+<br>
+/users/edit : Edit user<br>
+/users/delete : Delete user<br>
+<br>
+/videos/watch : Watch Video<br>
+/videos/edit : Edit Video<br>
+/videos/delete : Delete Video<br>
+/videos/comments : Comment on a video<br>
+/videos/comments/delete : Delete A Comment of a Video 
--- src/server.js
+++ src/server.js
@@ -5,20 +5,25 @@ const PORT = 4000;
 
 const app = express();
 const morganMiddleware = morgan("dev");
-
-const controllerHome = (req, res) => { 
-    return res.end("Response Here Home");
-}
-const controllerLogin = (req, res) => {
-    return res.end("Login Here");
-}
-const controllerProtect = (req, res) =>{
-    return res.end("Welcome to Protected Lounge")
-}
-
 app.use(morganMiddleware);
-app.get("/", controllerHome);
-app.get("/login", controllerLogin);
+
+// Global Router 생성
+const globalRouter = express.Router();
+const handleHome = (req, res) => res.send("Home");
+globalRouter.get("/", handleHome);
+
+const userRouter = express.Router();
+const handleEditUser = (req, res) => res.send("Edit User");
+userRouter.get("/edit", handleEditUser);
+
+const videoRouter = express.Router();
+const handleWatchVideo = (req, res) => res.send("Watch video");
+videoRouter.get("/watch", handleWatchVideo);
+
+app.use("/",globalRouter);
+app.use("/videos", videoRouter);
+app.use("/users", userRouter);
+
 
 const handleListening = () => {
     console.log(`🔨 Server Listen http://localhost:${PORT} 🐳`)
