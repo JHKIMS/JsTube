bb995ebe3aa09c8e0d37e3cf41afd4fe55267e24
JHKIMS <wjdgns5131@gmail.com>
2023-03-11 21:02:54 +0900

    JsTube: Router폴더로 따로 분리

--- src/server.js
+++ src/server.js
@@ -1,24 +1,14 @@
 import express from "express";
 import morgan from "morgan";
+import globalRouter from "./routers/globalRouter";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
 
 const PORT = 4000;
 
 const app = express();
 const morganMiddleware = morgan("dev");
 app.use(morganMiddleware);
-
-// Global Router 생성
-const globalRouter = express.Router();
-const handleHome = (req, res) => res.send("Home");
-globalRouter.get("/", handleHome);
-
-const userRouter = express.Router();
-const handleEditUser = (req, res) => res.send("Edit User");
-userRouter.get("/edit", handleEditUser);
-
-const videoRouter = express.Router();
-const handleWatchVideo = (req, res) => res.send("Watch video");
-videoRouter.get("/watch", handleWatchVideo);
 
 app.use("/",globalRouter);
 app.use("/videos", videoRouter);
