29276c7effebf5a96483212550452918684eec70
JHKIMS <wjdgns5131@gmail.com>
2023-03-12 16:30:29 +0900

    JsTube: Controller에서 pug로 변수 보내기

--- src/Controllers/videoController.js
+++ src/Controllers/videoController.js
@@ -1,12 +1,12 @@
 
-export const trending = (req, res) => res.render("home");
-
-export const watch = (req, res) => {
-    res.render("watch");
+export const trending = (req, res) => {
+    res.render("home", {pageTitle: "Home"});
 }
-
+export const watch = (req, res) => {
+    res.render("watch",{pageTitle:"Watch"});
+}
 export const edit = (req, res) => {
-    res.render("edit");
+    res.render("edit", {pageTitle:"Edit"});
 }
 export const search = (req, res) => res.send("Search");
 export const upload = (req, res) => res.send("Upload");
