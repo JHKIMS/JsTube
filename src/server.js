2c76213c494a3af9ccb74e7c8e12e770fdb1fce7
JHKIMS <wjdgns5131@gmail.com>
2023-03-12 15:29:11 +0900

    JsTube: Pug의 partial을 이용한 footer 공유

--- src/server.js
+++ src/server.js
@@ -9,7 +9,8 @@ const PORT = 4000;
 const app = express();
 const morganMiddleware = morgan("dev");
 
-app.set("View Engine", "pug"); // 우리가 pug을 쓴다는 것을 express에게 알려준다.
+app.set("view engine", "pug"); // 우리가 pug을 쓴다는 것을 express에게 알려준다.
+app.set("views", process.cwd() + "/src/views");
 app.use(morganMiddleware);
 app.use("/",globalRouter);
 app.use("/videos", videoRouter);
