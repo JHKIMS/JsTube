7c65ae125ca637dfc0ae306deed834002146e2d4
JHKIMS <wjdgns5131@gmail.com>
2023-03-13 08:52:03 +0900

    JsTube: EditVideo_Part2(POST)

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -5,8 +5,8 @@ const videoRouter = express.Router();
 
 videoRouter.get("/upload", upload);
 videoRouter.get("/:id(\\d+)", watch);
-videoRouter.get("/:id(\\d+)/edit", getEdit);
-videoRouter.post("/:id(\\d+)/edit", postEdit);
+videoRouter.route("/:id(\\d+)/edit").get(getEdit).post(postEdit);
+
 videoRouter.get("/:id(\\d+)/delete", deleteVideo);
 
 
--- src/server.js
+++ src/server.js
@@ -12,6 +12,7 @@ const morganMiddleware = morgan("dev");
 app.set("view engine", "pug"); // 우리가 pug을 쓴다는 것을 express에게 알려준다.
 app.set("views", process.cwd() + "/src/views");
 app.use(morganMiddleware);
+app.use(express.urlencoded({extended: true})) // express가 form의 value을 이해할 수 있게 한다. 우리가 사용할 수 있게 자바스크립트 형식으로 준다.
 app.use("/",globalRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
