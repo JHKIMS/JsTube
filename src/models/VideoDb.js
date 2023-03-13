a697ed37aa531153e176464aa3996ef01634c14f
JHKIMS <wjdgns5131@gmail.com>
2023-03-13 19:12:47 +0900

    JsTube: uploadVideo-VideoDb 설정2

--- src/models/VideoDb.js
+++ src/models/VideoDb.js
@@ -1,10 +1,10 @@
 import mongoose from "mongoose";
 
 const videoSchema = new mongoose.Schema({
-    title: {type: String, required: true},
-    description: {type: String, required: true},
+    title: {type: String, required: true, trim:true, maxLength:80},
+    description: {type: String, required: true, trim:true, minLength: 20},
     createdAt: {type:Date, required: true, default: Date.now}, // Date.now()에서 ()을 빼는 이유는 바로 실행하는걸 막기위해서.
-    hashtags: [{type:String}],
+    hashtags: [{type:String, trim: true}],
     meta:{
         views: {type:Number, default: 0, required: true},
         rating: {type:Number, default: 0, required: true},
