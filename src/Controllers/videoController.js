e678dfe41f29cee1a9bebd7b930e75667cddc526
JHKIMS <wjdgns5131@gmail.com>
2023-03-13 22:25:01 +0900

    JsTube: Edit 페이지 설정

--- src/Controllers/videoController.js
+++ src/Controllers/videoController.js
@@ -7,20 +7,28 @@ export const home = async (req, res) => {
 export const watch = async(req, res) => {
   const id = req.params.id; // Es6 const{ id } = req.params;
   const video = await Video.findById(id);
-  if(video){
-    return res.render("watch", { pageTitle: video.title, video });  
+  if(!video){ // 에러에 걸리는 부분을 if로 처리
+    return res.render("404", {pageTitle: "Video not found"});
   }
-  return res.render("404", {pageTitle: "Video not found"});
+  return res.render("watch", { pageTitle: video.title, video });  
 };
-export const getEdit = (req, res) => {
+
+
+export const getEdit = async(req, res) => {
   const id = req.params.id; // Es6 const{ id } = req.params;
-  return res.render("edit", { pageTitle: `Editing` });
+  const video = await Video.findById(id);
+  if(!video){
+    return res.render("404", { pageTitle: "Video not found"});  
+  }
+  return res.render("edit", { pageTitle: `Editing ${video.title}`, video });
 };
 export const postEdit = (req, res) => {
   const id = req.params.id; // Es6 const{ id } = req.params;
   const title = req.body.title;
   return res.redirect(`/videos/${id}`);
 };
+
+
 export const getUpload = (req, res) => {
   return res.render("upload", { pageTitle: "Upload Video" });
 };
