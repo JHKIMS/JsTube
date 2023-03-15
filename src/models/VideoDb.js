8c64a83675920979d874f5f4d6b1c17ffd3ce8a8
JHKIMS <wjdgns5131@gmail.com>
2023-03-16 08:33:11 +0900

    JsTube: uploadVideo hashtag-VideoDb에서 처리

--- src/models/VideoDb.js
+++ src/models/VideoDb.js
@@ -1,15 +1,22 @@
 import mongoose from "mongoose";
 
 const videoSchema = new mongoose.Schema({
-    title: {type: String, required: true, trim:true, maxLength:80},
-    description: {type: String, required: true, trim:true, minLength: 20},
-    createdAt: {type:Date, required: true, default: Date.now}, // Date.now()에서 ()을 빼는 이유는 바로 실행하는걸 막기위해서.
-    hashtags: [{type:String, trim: true}],
-    meta:{
-        views: {type:Number, default: 0, required: true},
-        rating: {type:Number, default: 0, required: true},
-    }
-})
+  title: { type: String, required: true, trim: true, maxLength: 80 },
+  description: { type: String, required: true, trim: true, minLength: 20 },
+  createdAt: { type: Date, required: true, default: Date.now }, // Date.now()에서 ()을 빼는 이유는 바로 실행하는걸 막기위해서.
+  hashtags: [{ type: String, trim: true }],
+  meta: {
+    views: { type: Number, default: 0, required: true },
+    rating: { type: Number, default: 0, required: true },
+  },
+});
+
+videoSchema.pre("save", async function () {
+  this.hashtags = this.hashtags[0]
+    .split(",")
+    .map((word) => (word.startsWith("#") ? word : `#${word}`));
+});
 
 const videoModel = mongoose.model("Video", videoSchema);
+
 export default videoModel;
