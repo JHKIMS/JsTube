0d6da084c1512d8f5c316d637bbbec26ec0c5c45
JHKIMS <wjdgns5131@gmail.com>
2023-03-17 10:31:31 +0900

    JsTube: StatusCode처리하기

--- src/Controllers/videoController.js
+++ src/Controllers/videoController.js
@@ -8,7 +8,7 @@ export const watch = async(req, res) => {
   const id = req.params.id; // Es6 const{ id } = req.params;
   const video = await Video.findById(id);
   if(!video){ // 에러에 걸리는 부분을 if로 처리
-    return res.render("404", {pageTitle: "Video not found"});
+    return res.status(404).render("404", {pageTitle: "Video not found"});
   }
   return res.render("watch", { pageTitle: video.title, video });  
 };
@@ -18,7 +18,7 @@ export const getEdit = async(req, res) => {
   const id = req.params.id; // Es6 const{ id } = req.params;
   const video = await Video.findById(id);
   if(!video){
-    return res.render("404", { pageTitle: "Video not found"});  
+    return res.status(404).render("404", { pageTitle: "Video not found"});  
   }
   return res.render("edit", { pageTitle: `Editing ${video.title}`, video });
 };
@@ -52,7 +52,7 @@ export const postUpload = async (req, res) => {
     return res.redirect("/");
   } catch (error) {
     console.log(error);
-    return res.render("upload", {
+    return res.status(400).render("upload", {
       pageTitle: "Upload Video",
       errorMessage: error._message,
     });
