017caff5bd5496bae5b163e130b73f1808eef7c1
JHKIMS <wjdgns5131@gmail.com>
2023-03-17 10:18:08 +0900

    JsTube: FormValidation-Username, Email, Password

--- src/Controllers/userController.js
+++ src/Controllers/userController.js
@@ -3,7 +3,21 @@ import User from "../models/UserDb";
 export const getJoin = (req, res) =>
   res.render("join", { pageTitle: "Join Account" });
 export const postJoin = async (req, res) => {
-  const { name, username,email, password, location } = req.body;
+  const { name, username, email, password, password2, location } = req.body;
+  const pageTitle="Join";
+  if(password !== password2){
+    return res.render("join", {
+        pageTitle,
+        errorMessage: "Password confirmation does not match",
+      });
+  }
+  const exists = await User.exists({ $or: [{username},{email}] });
+  if (exists) {
+    return res.render("join", {
+      pageTitle,
+      errorMessage: "Username or Email already exists",
+    });
+  }
   await User.create({
     name,
     username,
