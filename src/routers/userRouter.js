def450c19c1e795a6ef33c7683d6941747fa6f34
JHKIMS <wjdgns5131@gmail.com>
2023-03-18 08:52:28 +0900

    JsTube: Github_Oauth구현-2

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,11 +1,12 @@
 import express from "express";
-import { editUser, deleteUser, see, logout, startGithubLogin } from "../Controllers/userController";
+import { editUser, deleteUser, see, logout, startGithubLogin, finishGithubLogin } from "../Controllers/userController";
 
 const userRouter = express.Router();
 
 userRouter.get("/edit", editUser);
 userRouter.get("/delete", deleteUser);
-userRouter.get("/github/start", startGithubLogin)
+userRouter.get("/github/start", startGithubLogin);
+userRouter.get("/github/finish", finishGithubLogin);
 userRouter.get(":id", see);
 userRouter.get("/logout", logout);
 
