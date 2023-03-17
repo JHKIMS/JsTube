d3ebfa8c2b31735de8657836e7ced261ebdcf8c0
JHKIMS <wjdgns5131@gmail.com>
2023-03-17 16:11:40 +0900

    JsTube: MongoStore(connect-mongo 설치)-세션DB에 저장

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import morgan from "morgan";
+import MongoStore from "connect-mongo";
 import rootRouter from "./routers/rootRouter";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
@@ -12,29 +13,33 @@ const morganMiddleware = morgan("dev");
 app.set("view engine", "pug"); // 우리가 pug을 쓴다는 것을 express에게 알려준다.
 app.set("views", process.cwd() + "/src/views");
 app.use(morganMiddleware);
-app.use(express.urlencoded({extended: true})) // express가 form의 value을 이해할 수 있게 한다. 우리가 사용할 수 있게 자바스크립트 형식으로 준다.
+app.use(express.urlencoded({ extended: true })); // express가 form의 value을 이해할 수 있게 한다. 우리가 사용할 수 있게 자바스크립트 형식으로 준다.
 
-app.use(session({
+app.use(
+  session({
     secret: "Hello",
     resave: true,
     saveUninitialized: true,
-}));
+    store: MongoStore.create({
+        mongoUrl: "mongodb://127.0.0.1:27017/tube"
+    }) // 세션이 DB에 저장되어 있기 때문에 서버를 껏다켜도 로그인이 유지된다.
+  })
+);
 
-app.use((req,res,next) =>{
-    req.sessionStore.all((eroor, sessions)=>{
-        console.log(sessions);
-        next();
-    })
-})
+app.use((req, res, next) => {
+  req.sessionStore.all((eroor, sessions) => {
+    console.log(sessions);
+    next();
+  });
+});
 
-app.get("/add-one",(req,res,next) => {
-    req.session.potato += 1;
-    return res.send(`${req.session.id}`);
-})
+app.get("/add-one", (req, res, next) => {
+  req.session.potato += 1;
+  return res.send(`${req.session.id}`);
+});
 app.use(localMiddleware);
-app.use("/",rootRouter);
+app.use("/", rootRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
 
 export default app;
-
