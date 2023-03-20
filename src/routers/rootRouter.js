a6860aa93b8938c990e74920d9f09f48ecb3d3d5
JHKIMS <wjdgns5131@gmail.com>
2023-03-20 14:57:36 +0900

    JsTube: Protect,Public Middleware

--- src/routers/rootRouter.js
+++ src/routers/rootRouter.js
@@ -1,6 +1,12 @@
 import express from "express";
-import {getJoin, getLogin, postJoin, postLogin} from "../Controllers/userController";
-import {search, home} from "../Controllers/videoController";
+import {
+  getJoin,
+  getLogin,
+  postJoin,
+  postLogin,
+} from "../Controllers/userController";
+import { search, home } from "../Controllers/videoController";
+import { publicOnlyMiddleware } from "../middleware";
 
 const rootRouter = express.Router();
 
@@ -8,9 +14,12 @@ const handleHome = (req, res) => res.send("Home");
 const handleJoin = (req, res) => res.send("Join");
 
 rootRouter.get("/", home);
-rootRouter.route("/join").get(getJoin).post(postJoin);
-rootRouter.route("/login").get(getLogin).post(postLogin);
+rootRouter.route("/join").all(publicOnlyMiddleware).get(getJoin).post(postJoin);
+rootRouter
+  .route("/login")
+  .all(publicOnlyMiddleware)
+  .get(getLogin)
+  .post(postLogin);
 rootRouter.get("/search", search);
 
 export default rootRouter;
-
--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,12 +1,26 @@
 import express from "express";
-import { editUser, deleteUser, see, logout, startGithubLogin, finishGithubLogin, getEditUser, postEditUser } from "../Controllers/userController";
+import {
+  editUser,
+  deleteUser,
+  see,
+  logout,
+  startGithubLogin,
+  finishGithubLogin,
+  getEditUser,
+  postEditUser,
+} from "../Controllers/userController";
+import { protectorMiddleware, publicOnlyMiddleware } from "../middleware";
 
 const userRouter = express.Router();
 
-userRouter.route("/edit").get(getEditUser).post(postEditUser);
-userRouter.get("/github/start", startGithubLogin);
-userRouter.get("/github/finish", finishGithubLogin);
+userRouter
+  .route("/edit")
+  .all(protectorMiddleware)
+  .get(getEditUser)
+  .post(postEditUser);
+userRouter.get("/github/start",publicOnlyMiddleware,startGithubLogin);
+userRouter.get("/github/finish",publicOnlyMiddleware, finishGithubLogin);
 userRouter.get(":id", see);
-userRouter.get("/logout", logout);
+userRouter.get("/logout", protectorMiddleware, logout);
 
 export default userRouter;
