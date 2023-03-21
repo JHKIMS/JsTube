e7eade3479274b850d278290362172d37fd9b9da
JHKIMS <wjdgns5131@gmail.com>
2023-03-22 00:50:34 +0900

    JsTube: Webpack-Config3

--- src/server.js
+++ src/server.js
@@ -43,6 +43,7 @@ app.get("/add-one", (req, res, next) => {
 });
 app.use(localMiddleware);
 app.use("/uploads", express.static("uploads"));
+app.use("/assets", express.static("assets"));
 app.use("/", rootRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
