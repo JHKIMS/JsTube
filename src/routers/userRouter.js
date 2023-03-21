38f58567cb3efd24ff5af71432d6f9589a3e9943
JHKIMS <wjdgns5131@gmail.com>
2023-03-21 09:10:21 +0900

    JsTube: UserProfile페이지 생성

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -23,7 +23,6 @@ userRouter
 
 userRouter.get("/github/start", publicOnlyMiddleware, startGithubLogin);
 userRouter.get("/github/finish", publicOnlyMiddleware, finishGithubLogin);
-userRouter.get(":id", see);
 userRouter.get("/logout", protectorMiddleware, logout);
 
 userRouter
@@ -31,5 +30,7 @@ userRouter
   .all(protectorMiddleware)
   .get(getChangePassword)
   .post(postChangePassword);
+
+  userRouter.get("/:id", see);
 
 export default userRouter;
