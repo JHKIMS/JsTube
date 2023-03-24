c8a6cfae96c2d17721ced883e8cc050c05ce89ef
JHKIMS <wjdgns5131@gmail.com>
2023-03-24 19:48:22 +0900

    JsTube: 비디오녹화-미리보기 구현

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -1,9 +1,12 @@
 const startBtn = document.getElementById("startBtn");
+const videoPreview = document.getElementById("preview");
 
 const handleStart = async () => {
   const stream = await navigator.mediaDevices.getUserMedia({
     audio: true,
     video: true,
   });
+  videoPreview.srcObject = stream;
+  videoPreview.play();
 };
 startBtn.addEventListener("click", handleStart);
