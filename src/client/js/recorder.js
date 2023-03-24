cfd4144e356fac0c610a89ba877cee69d0b6e18e
JHKIMS <wjdgns5131@gmail.com>
2023-03-24 20:14:45 +0900

    JsTube: 비디오녹화 기능 구현중-1

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -1,12 +1,36 @@
 const startBtn = document.getElementById("startBtn");
 const videoPreview = document.getElementById("preview");
 
-const handleStart = async () => {
-  const stream = await navigator.mediaDevices.getUserMedia({
+let stream;
+
+const handleStop = () =>{
+    startBtn.innerText = "Start Recording";
+    startBtn.removeEventListener("click", handleStop);
+    startBtn.addEventListener("click", handleStart);
+}
+const handleStart = () =>{
+    startBtn.innerText = "Stop Recording";
+    startBtn.removeEventListener("click", handleStart);
+    startBtn.addEventListener("click", handleStop);
+
+    const recorder = new MediaRecorder(stream);
+    recorder.ondataavailable = (event) => console.log(e);
+    recorder.start();
+    setTimeout(() =>{
+        recorder.stop();
+    }, 10000);
+}
+
+const init = async () => {
+ stream = await navigator.mediaDevices.getUserMedia({
     audio: true,
     video: true,
   });
   videoPreview.srcObject = stream;
   videoPreview.play();
 };
+
+init();
+
+
 startBtn.addEventListener("click", handleStart);
