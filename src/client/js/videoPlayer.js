ffaaec2cfb4d5db44409f7a1baaff5b01c3a93fd
JHKIMS <wjdgns5131@gmail.com>
2023-03-24 10:28:55 +0900

    JsTube: Video 재생시간 만들기

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -3,8 +3,9 @@ import videoModel from "../../models/VideoDb";
 const video = document.querySelector("video");
 const playBtn = document.getElementById("play");
 const muteBtn = document.getElementById("mute");
-const time = document.getElementById("time");
 const volumeRange = document.getElementById("volume");
+const currentTime = document.getElementById("currentTime");
+const totalTime = document.getElementById("totalTime");
 
 let volumeValue = 0.5;
 video.volume = volumeValue;
@@ -38,8 +39,18 @@ const handleVolumeChange=(event) =>{
     video.volume = value;
 }
 
+const handleLoadMetadata = () =>{
+    totalTime.innerText = Math.floor(video.duration);
+}
+const handleTimeUpdate = () =>{
+    currentTime.innerText = Math.floor(video.currentTime);
+}
+
 playBtn.addEventListener("click", handlePlayClick);
 muteBtn.addEventListener("click", handleMute);
 
 volumeRange.addEventListener("input", handleVolumeChange);
+
+video.addEventListener("loadedmetadata", handleLoadMetadata);
+video.addEventListener("timeupdate", handleTimeUpdate);
 
