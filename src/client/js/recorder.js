96632ef7ad79822c97eb3b7988b3987136976bf5
JHKIMS <wjdgns5131@gmail.com>
2023-03-25 09:34:05 +0900

    JsTube: 코드정리 및 Record Again

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -1,6 +1,6 @@
 import { createFFmpeg,fetchFile } from "@ffmpeg/ffmpeg";
 
-const startBtn = document.getElementById("startBtn");
+const actionBtn = document.getElementById("actionBtn");
 const videoPreview = document.getElementById("preview");
 
 let stream;
@@ -12,8 +12,20 @@ const files = {
     output: "convert-output.mp4",
     thumb: "thumbnail.jpg",
 }
+const downloadFile = (fileUrl, fileName) => {
+    const a = document.createElement("a");
+    a.href=fileUrl;
+    a.download = fileName;
+    document.body.appendChild(a);
+    a.click();
+}
 
 const handleDownload = async() =>{
+    actionBtn.removeEventListener("click", handleDownload);
+
+    actionBtn.innerText = "Transcoding~";
+    actionBtn.disabled = true;
+
     const ffmpeg = createFFmpeg({log: true});
     await ffmpeg.load();
 
@@ -43,34 +55,34 @@ const handleDownload = async() =>{
     const mp4Url = URL.createObjectURL(mp4Blob);
     const thumbUrl = URL.createObjectURL(thumbBlob);
 
-    const a = document.createElement("a");
-    a.href=mp4Url;
-    a.download = "MyRecording.mp4";
-    document.body.appendChild(a);
-    a.click();
-
-    const thumbA = document.createElement("a");
-    thumbA.href=thumbUrl;
-    thumbA.download = "MyThumbnail.jpg";
-    document.body.appendChild(thumbA);
-    thumbA.click();
+    downloadFile(mp4Url, "MyRecording.mp4");
+    downloadFile(thumbUrl, "MyThumbnail.jpg");
 
     // FFmpeg로 변환작업을 수행할 때 임시 파일을 삭제하기 위함(속도+저장공간 확보)
     ffmpeg.FS("unlink", files.input);
     ffmpeg.FS("unlink", files.output);
     ffmpeg.FS("unlink", files.thumb);
+
+    URL.revokeObjectURL(mp4Url);
+    URL.revokeObjectURL(thumbUrl);
+    URL.revokeObjectURL(videoFile);
+
+    actionBtn.disabled = false;
+    init();
+    actionBtn.innerText = "Record Again";
+    actionBtn.addEventListener("click", handleStart);
 }
 const handleStop = () =>{
-    startBtn.innerText = "Download Recording";
-    startBtn.removeEventListener("click", handleStop);
-    startBtn.addEventListener("click", handleDownload);
+    actionBtn.innerText = "Download Recording";
+    actionBtn.removeEventListener("click", handleStop);
+    actionBtn.addEventListener("click", handleDownload);
 
     recorder.stop();
 }
 const handleStart = () =>{
-    startBtn.innerText = "Stop Recording";
-    startBtn.removeEventListener("click", handleStart);
-    startBtn.addEventListener("click", handleStop);
+    actionBtn.innerText = "Stop Recording";
+    actionBtn.removeEventListener("click", handleStart);
+    actionBtn.addEventListener("click", handleStop);
 
     recorder = new MediaRecorder(stream);
     recorder.ondataavailable = (event) => {
@@ -94,4 +106,4 @@ const init = async () => {
 init();
 
 
-startBtn.addEventListener("click", handleStart);
+actionBtn.addEventListener("click", handleStart);
